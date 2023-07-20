4e22961137ca7d200f72438962811e265f908dce
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-07-20 19:07:56 +0300

    Create ExpenseDate.css

--- src/App.js
+++ src/App.js
@@ -34,26 +34,26 @@ function App() {
         id={expenses[0].id}
         title={expenses[0].title}
         amount={expenses[0].amount}
-        date={expenses[0].date }>
-      </ExpenseItem>
+        date={expenses[0].date }
+      />
       <ExpenseItem  
         id={expenses[1].id}
         title={expenses[1].title}
         amount={expenses[1].amount}
-        date={expenses[1].date }>
-      </ExpenseItem>
+        date={expenses[1].date }
+      />
       <ExpenseItem  
         id={expenses[2].id}
         title={expenses[2].title}
         amount={expenses[2].amount}
-        date={expenses[2].date }>
-      </ExpenseItem>
+        date={expenses[2].date }
+      />
       <ExpenseItem 
         id={expenses[3].id}
         title={expenses[3].title}
         amount={expenses[3].amount}
-        date={expenses[3].date }>
-      </ExpenseItem>
+        date={expenses[3].date }
+      />
     </div>
   );
 }
