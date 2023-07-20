9f9fc7ed48dff7ef3c331cfc2602281fda8ca0f6
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-07-20 16:05:36 +0300

    Update App.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date(2023, 2, 28);
+    const expenseTitle = 'Car ınsurance';
+    const expenseAmount = 294.67;
+
     return (
         <div className='expense-item'>
-            <div>March 28th, 2023</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>Car Insurance</h2>
-                <div className='expense-item__price'>$294.67</div>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>${expenseAmount}</div>
             </div>
         </div>
     )
