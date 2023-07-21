67871f80ab397d9f1a20c0c0c64271ddd041fdb2
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-07-21 18:24:31 +0300

    Create Card wrapper component

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,58 +1,35 @@
 import ExpenseItem from "./ExpenseItem";
+import './Expenses.css';
 
-function Expenses() {
-    const expenses = [
-        {
-          id: 'e1',
-          title: 'Toilet Paper',
-          amount: 94.12,
-          date: new Date(2020, 7, 14),
-        },
-        { id: 'e2', 
-          title: 'New TV', 
-          amount: 799.49, 
-          date: new Date(2021, 2, 12) },
-        {
-          id: 'e3',
-          title: 'Car Insurance',
-          amount: 294.67,
-          date: new Date(2021, 2, 28),
-        },
-        {
-          id: 'e4',
-          title: 'New Desk (Wooden)',
-          amount: 450,
-          date: new Date(2021, 5, 12),
-        }
-      ];
+function Expenses(props) {
 
       return (
-        <div>
+        <div className='expenses'>
             <ExpenseItem 
-                id={expenses[0].id}
-                title={expenses[0].title}
-                amount={expenses[0].amount}
-                date={expenses[0].date }
+                id={props.items[0].id}
+                title={props.items[0].title}
+                amount={props.items[0].amount}
+                date={props.items[0].date }
             />
             <ExpenseItem  
-                id={expenses[1].id}
-                title={expenses[1].title}
-                amount={expenses[1].amount}
-                date={expenses[1].date }
+                id={props.items[1].id}
+                title={props.items[1].title}
+                amount={props.items[1].amount}
+                date={props.items[1].date }
             />
             <ExpenseItem  
-                id={expenses[2].id}
-                title={expenses[2].title}
-                amount={expenses[2].amount}
-                date={expenses[2].date }
+                id={props.items[2].id}
+                title={props.items[2].title}
+                amount={props.items[2].amount}
+                date={props.items[2].date }
             />
             <ExpenseItem 
-                id={expenses[3].id}
-                title={expenses[3].title}
-                amount={expenses[3].amount}
-                date={expenses[3].date }
+                id={props.items[3].id}
+                title={props.items[3].title}
+                amount={props.items[3].amount}
+                date={props.items[3].date }
             />
-      </div>
+        </div>
       );
 }
 
