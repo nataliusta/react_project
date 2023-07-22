af35c8cf33374ae1684e6d391529b662662f6231
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-07-22 16:57:42 +0300

    Add button to ExpensesItem.js

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -12,6 +12,7 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button>Change title</button>
         </Card>
     );
 }
