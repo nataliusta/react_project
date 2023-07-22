bd4bfb6a68fc64fdf3c3f82b78f6077a9c278c9b
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-07-22 12:00:11 +0300

    Create Expenses and UI folders

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ExpenseItem from "./ExpenseItem";
-import Card from './Card';
+import Card from '../UI/Card';
 import './Expenses.css';
 
 function Expenses(props) {
