5740ae9470a5918e6e1fe96ae58f0eda329745a2
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-07-23 14:09:12 +0300

    Call useState function in ExpensesItem

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+
+    useState();
+
+    const clickHandler = () => {
+        console.log('Hello!');
+    }
 
     return (
         <Card className='expense-item'>
@@ -12,7 +18,7 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-            <button>Change title</button>
+            <button onClick={clickHandler()}>Change title</button>
         </Card>
     );
 }
