d3f371f59679bcf9e5c8f310bc70b825f549b717
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-07-25 18:41:47 +0300

    Update state in ExpenseForm.js

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -7,31 +7,52 @@ const ExpenseForm = () => {
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
 
+    //const [userInput, setUserInput] = useState({
+    //    enteredTitle: '',
+    //    enteredAmount: '',
+    //    enteredDate: ''
+    //});
+
     const titleChangeHandler = (event) => {
         setEnteredTitle(event.target.value);
+        //setUserInput({
+        //   ...userInput,
+        //   enteredTitle: event.target.value,
+        //})
+        //setUserInput((prevState) => {
+        //    return {...prevState, enteredTitle: event.target.value}; // when state update depends on the prev state
+        //});
     };
 
     const amountChangeHandler = (event) => {
         setEnteredAmount(event.target.value);
+       // setUserInput({
+        //    ...userInput,
+        //   enteredAmount: event.target.value,
+        //})
     };
 
     const dateChangeHandler = (event) => {
         setEnteredDate(event.target.value);
+        //setUserInput({
+        //    ...userInput,
+        //    enteredDate: event.target.value,
+        //})
     };
 
     return (
         <form>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
-                    <label>Title</label>
+                    <label>{enteredTitle}</label>
                     <input type='text' onChange={titleChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
-                    <label>Amount</label>
+                    <label>{enteredAmount}</label>
                     <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
-                    <label>Date</label>
+                    <label>{enteredDate}</label>
                     <input type='date' min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler}/>
                 </div>
             </div>
