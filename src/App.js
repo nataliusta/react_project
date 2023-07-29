cc5783f0ca0096726d69754410473b1e506726ff
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-07-29 15:21:07 +0300

    Update NewExpense.js

--- src/App.js
+++ src/App.js
@@ -28,9 +28,14 @@ const App = () => {
     }
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expenses);
+  };
+
   return (
     <div>
-        <NewExpense />
+        <NewExpense onAddExpense={addExpenseHandler}/>
         <Expenses items={expenses}/>
     </div>
   );
