b075573ab85cd359922aa01902748508306d9cb0
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-07-31 13:55:24 +0300

    Add map method to Expenses.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,31 +14,17 @@ const Expenses = (props) => {
       return (
         <div>
             <Card className='expenses'>
-                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                <ExpenseItem 
-                    id={props.items[0].id}
-                    title={props.items[0].title}
-                    amount={props.items[0].amount}
-                    date={props.items[0].date }
+                <ExpensesFilter 
+                    selected={filteredYear} 
+                    onChangeFilter={filterChangeHandler} 
                 />
-                <ExpenseItem  
-                    id={props.items[1].id}
-                    title={props.items[1].title}
-                    amount={props.items[1].amount}
-                    date={props.items[1].date }
-                />
-                <ExpenseItem  
-                    id={props.items[2].id}
-                    title={props.items[2].title}
-                    amount={props.items[2].amount}
-                    date={props.items[2].date }
-                />
-                <ExpenseItem 
-                    id={props.items[3].id}
-                    title={props.items[3].title}
-                    amount={props.items[3].amount}
-                    date={props.items[3].date }
-                />
+                {props.items.map((expense) => (
+                    <ExpenseItem 
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                ))}
             </Card>
         </div>
       );
