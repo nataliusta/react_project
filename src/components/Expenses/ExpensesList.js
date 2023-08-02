c8a023903018f1a67d835ee0d4e557fc4888afe8
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-02 16:10:56 +0300

    Add button Cancel to ExpensesForm.js

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -3,16 +3,11 @@ import ExpenseItem from "./ExpenseItem";
 import './ExpensesList.css';
 
 const ExpensesList = (props) => {
-    let expensesContent = <p>No expenses found</p>;
 
     if (props.items.length === 0) {
         return <h2 className='expenses-list__fallback'>Found no expenses</h2>
     }
-
-    if (props.items.length > 0) {
-        expensesContent = ;
-    }
-
+    
     return <ul className='expenses-list'>
         {props.items.map((expense) => (
             <ExpenseItem 
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -61,6 +61,7 @@ const ExpenseForm = (props) => {
                 </div>
             </div>
             <div className='new-expense__actions'>
+                <button type='submit'>Cancel</button>
                 <button type='submit'>Add Expense</button>
             </div>
         </form>
