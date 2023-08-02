3297a3c79b23414ea95b0881d88d4d4bfedff6c3
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-02 23:08:01 +0300

    Update Chart.js

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,6 +3,8 @@ import ChartBar from './ChartBar';
 import './Chart.css';
 
 const Chart = (props) => {
+    const dataPointValues = props.dataPoint.map(dataPoint => dataPoint.value);
+    const totalMaximum = Math.max(...dataPointValues);
     return (
         <div className='chart'>
             {props.dataPoints.map(dataPoint => (
