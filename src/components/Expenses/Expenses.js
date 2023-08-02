689758428a8cc60ec28b95df461d88a49ca5cb5b
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-02 23:13:03 +0300

    Add a new chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -22,6 +23,7 @@ const Expenses = (props) => {
                     selected={filteredYear} 
                     onChangeFilter={filterChangeHandler} 
                 />
+                <ExpensesChart expenses={filteredExpenses}/>
                 <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -18,7 +18,7 @@ const ExpensesChart = (props) => {
         { label: 'Dec', value: 0 }
     ];
 
-    for (const expense in props.expenses) {
+    for (const expense of props.expenses) {
         const expenseMonth = expense.date.getMonth(); // starting at 0 => January => 0
         chartDataPoints[expenseMonth].value += expense.amount;
     }
